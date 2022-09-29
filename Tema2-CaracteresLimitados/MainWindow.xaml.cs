1832ca058c28c3f04458d2f6472f7575dd5baa8f
EduardoMoraton <90452631+EduardoMoraton@users.noreply.github.com>
2022-09-29 20:00:03 +0200

    Agregar archivos de proyecto.

---
+++ Tema2-CaracteresLimitados/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tema2_CaracteresLimitados
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void mainTextTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int numOfChars;
+
+            numOfChars = mainTextTextBox.Text.Length;
+            indicatorTextBlock.Text = numOfChars.ToString() + " / 140";
+
+            if (numOfChars >= 140)
+                mainTextTextBox.IsReadOnly = true;
+            else
+                mainTextTextBox.IsReadOnly = false;
+
+        }
+    }
+}
